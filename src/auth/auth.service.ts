f252006f1b2c9cca45825dd37addf055edac1bde
Sayantan <sayantan.dps@gmail.com>
2022-09-27 10:51:08 +0530

    Added DTO

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+// import * as argon from 'argon2'
 
-@Injectable({})
+@Injectable()
 export class AuthService {
-  signup() {
-    return 'I am signed up';
+  constructor(private prisma: PrismaService) {}
+  signup(dto: AuthDto) {
+    // generate the password hash
+    // save the new user in the db
+    // return the saved user
   }
 
   signin() {
