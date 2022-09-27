f704e3e22bbd174174d2c147201398238ddcf671
Sayantan Mondal <sayantan.dps@gmail.com>
2022-09-27 07:10:37 +0000

    Additions done

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
-// import * as argon from 'argon2'
+import * as argon from 'argon2'
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signup(dto: AuthDto) {
+  async signup(dto: AuthDto) {
     // generate the password hash
+    const hash = await argon.hash(dto.password);
     // save the new user in the db
+    const user =  await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+      // select: {
+      //   id: true,
+      //   email: true,
+      //   createdAt: true,
+      // }
+    })
+    delete user.hash; // Remove the hash field from user response body
     // return the saved user
+    return user;
   }
 
   signin() {
